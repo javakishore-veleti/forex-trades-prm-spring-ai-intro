8860b49cb708812c2f8db70e6e0bb9550c064caa
Kishore <>
2025-04-01 08:10:27 -0400

    Initial commit

---
+++ src/main/java/com/jk/ref_impls/trades/forex/prm/spring_ai/indexing/RAGTikaDocumentReader.java
@@ -0,0 +1,17 @@
+package com.jk.ref_impls.trades.forex.prm.spring_ai.indexing;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class RAGTikaDocumentReader {
+
+    public List<Document> readFromt(Resource resource) {
+        var tikaDocumentReader = new TikaDocumentReader(resource);
+        return tikaDocumentReader.read();
+    }
+}
